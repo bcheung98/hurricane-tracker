b159e108a7e6cf6d0ba01936cd7e8d29b8bec184
Brian <briantcheung@gmail.com>
2021-12-22 16:28:55 -0800

    Stylized app bar

--- src/components/TrackViewer.js
+++ src/components/TrackViewer.js
@@ -1,9 +1,33 @@
 import React from "react";
 
-import { Button, Box, InputLabel, MenuItem, FormControlLabel, FormControl, Select, Radio, RadioGroup, AppBar, Toolbar, Typography } from "@mui/material";
+import { Button, Box, InputBase, MenuItem, FormControlLabel, FormControl, Select, Radio, RadioGroup, AppBar, Toolbar, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+    'label + &': {
+        marginTop: theme.spacing(3),
+    },
+    '& .MuiInputBase-input': {
+        borderRadius: 4,
+        position: 'relative',
+        backgroundColor: "white",
+        border: '1px solid #ced4da',
+        fontSize: 16,
+        padding: '10px 26px 10px 12px',
+        '&:focus': {
+            borderRadius: 4,
+            borderColor: '#80bdff',
+            boxShadow: '0 0 0 0.2rem rgba(0,12,255,.25)',
+            backgroundColor: "white",
+        },
+    },
+}));
 
 const TrackViewer = () => {
-    let years = [2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999, 1998, 1997, 1996, 1995, 1994, 1993, 1992, 1991, 1990]
+
+    // Generates an array of numbers
+    const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step));
+
     const [year, setYear] = React.useState("");
     const handleYearChange = (e) => {
         setYear(e.target.value);
@@ -28,25 +52,28 @@ const TrackViewer = () => {
                             Storm Tracker
                         </Typography>
                         <form onSubmit={handleSubmit}>
-                            <FormControl>
-                                <RadioGroup sx={{marginLeft: "30px" }} row defaultValue="atlantic" onChange={handleRadioChange}>
+                            <FormControl sx={{ marginLeft: "30px", marginTop: "9px" }}>
+                                <RadioGroup row defaultValue="atlantic" onChange={handleRadioChange}>
                                     <FormControlLabel value="atlantic" control={<Radio color="default" />} label="Atlantic" />
                                     <FormControlLabel value="pacific" control={<Radio color="default" />} label="Pacific" />
                                 </RadioGroup>
                             </FormControl>
-                            <FormControl sx={{ width: "250px", marginLeft: "20px", marginRight: "20px", marginTop: "5px" }}>
-                                <InputLabel>Year</InputLabel>
+                            <FormControl sx={{ m: 1, width: "100px" }} variant="standard">
+
                                 <Select
-                                    sx={{ height: "35px" }}
                                     value={year}
-                                    label="year"
                                     onChange={handleYearChange}
+                                    displayEmpty
+                                    input={<BootstrapInput />}
                                     required
                                 >
-                                    {years.map(year => <MenuItem key={year} value={year}>{year}</MenuItem>)}
+                                    <MenuItem value="">
+                                        <em>Year</em>
+                                    </MenuItem>
+                                    {range(1990, 2021, 1).reverse().map(year => <MenuItem key={year} value={year}>{year}</MenuItem>)}
                                 </Select>
                             </FormControl>
-                            <Button sx={{ marginTop: "3px" }} type="submit" variant="contained" color="secondary">Go</Button>
+                            <Button sx={{ marginTop: "12px", marginLeft: "10px" }} type="submit" variant="contained" color="secondary">Go</Button>
                         </form>
                     </Toolbar>
                 </AppBar>
